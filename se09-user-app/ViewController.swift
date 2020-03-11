4f8f9331450784bda95744a601c9c1a9629ec9b8
henrikengelbrink <henrik@keeet.io>
2020-03-11 21:01:48 +0100

    Initial Commit

---
+++ se09-user-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  se09-user-app
+//
+//  Created by Henrik Engelbrink on 11.03.20.
+//  Copyright © 2020 Henrik Engelbrink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ se09-user-appTests/se09_user_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  se09_user_appTests.swift
+//  se09-user-appTests
+//
+//  Created by Henrik Engelbrink on 11.03.20.
+//  Copyright © 2020 Henrik Engelbrink. All rights reserved.
+//
+
+import XCTest
+@testable import se09_user_app
+
+class se09_user_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
