e9aa58ef4c058a9db70a9d3bfa70151dba377994
henrikengelbrink <henrik@keeet.io>
2020-03-19 00:35:27 +0100

    feat: Refresh token

--- se09-user-app/Service/AuthService.swift
+++ se09-user-app/Service/AuthService.swift
@@ -8,13 +8,12 @@ class AuthService {
     private let authEndpoint = URL(string: "https://api.engelbrink.dev/hydra/oauth2/auth")!
     private let tokenEndpoint = URL(string: "https://api.engelbrink.dev/hydra/oauth2/token")!
     private let redirectURL = URL(string: "com.example-app:/oauth2/callback")!
-    private let clientId = "test-client"
-    private let clientSecret = "test-secret"
+    private let clientId = "74c1bf4d626d392e5c6b4faaa4427e6a"
+    private let clientSecret = "fe5d7aa261650f2d2793cf86acc8b34bf516574f4b1955c468eaefe113b848d8"
     private lazy var config = OIDServiceConfiguration(authorizationEndpoint: authEndpoint, tokenEndpoint: tokenEndpoint)
     
-    private let keychainName = "com.se09.userapp"
-    private let keychainKeyToken = "keychainKeyToken"
-    private let keychainKeyRefresh = "keychainKeyRefresh"
+    private let keychainKeyAuthState = "keychainKeyAuthState"
+    private let keychain = Keychain(service: "com.se09.userapp")
     
     func authorize(from vc: UIViewController, onSuccess: @escaping (OIDAuthState) -> Void, onError: @escaping (Error) -> Void) {
         let request = OIDAuthorizationRequest(configuration: config,
@@ -25,14 +24,8 @@ class AuthService {
                                               responseType: OIDResponseTypeCode,
                                               additionalParameters: nil)
         currentAuthFlow = OIDAuthState.authState(byPresenting: request, presenting: vc, callback: { (authState, error) in
-            authState?.tokenRefreshRequest()
-            if let authState != authState {
-                
-                print("Refresh \(authState.refreshToken!)")
-                print("Scope \(authState.scope!)")
-                print("AccessToken \(authState.lastTokenResponse!.accessToken!)")
-                print("ExpDate \(authState.lastTokenResponse!.accessTokenExpirationDate!)")
-                self.saveLoginData(token: authState.lastTokenResponse!.accessToken!, refresh: authState.refreshToken!)
+            if let authState = authState {
+                self.saveAuthState(authState: authState)
                 onSuccess(authState)
             } else if let error = error {
                 print(error)
@@ -40,36 +33,45 @@ class AuthService {
             }
         })
     }
-    
-    func refreshToken() {
-        print("refreshToken")
-        let request = OIDAuthorizationRequest(
-            configuration: config,
-            clientId: clientId,
-            clientSecret: clientSecret,
-            scopes: [OIDScopeOpenID, "offline"],
-            redirectURL: redirectURL,
-            responseType: OIDResponseTypeCode,
-            additionalParameters: nil
-        )
-        let response = OIDAuthorizationResponse(request: request, parameters: [:])
-        let state = OIDAuthState(authorizationResponse: response)
-        let oIDTokenRequest = OIDAuthState.tokenRefreshRequest(state)
-        print(oIDTokenRequest)
+
+    private func saveAuthState(authState: OIDAuthState) {
+        let authStateData = try? NSKeyedArchiver.archivedData(withRootObject: authState, requiringSecureCoding: false)
+        try? self.keychain.set(authStateData!, key: keychainKeyAuthState)
     }
-    
-    private func saveLoginData(token: String, refresh: String) {
-        let keychain = Keychain(service: keychainName)
-        try? keychain.set(token, key: keychainKeyToken)
-        try? keychain.set(refresh, key: keychainKeyRefresh)
-    }
+
     
     func userLoggedIn() -> Bool {
         var loggedIn = true
-        let keychain = Keychain(service: keychainName)
-        let token = try? keychain.get(keychainKeyToken)
-        let refresh = try? keychain.get(keychainKeyRefresh)
-        if token == nil || refresh == nil {
+        let authStateData: Data? = try? keychain.getData(keychainKeyAuthState)
+        if authStateData != nil {
+            if let authState = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(authStateData!) as? OIDAuthState {
+                if authState.lastTokenResponse != nil && authState.lastTokenResponse!.accessTokenExpirationDate != nil {
+                    if authState.lastTokenResponse!.accessTokenExpirationDate!.timeIntervalSince1970 < Date.init().timeIntervalSince1970 {
+                        print("REFRESH TOKEN")
+                        let request = authState.lastAuthorizationResponse.tokenExchangeRequest()
+                        OIDAuthorizationService.perform(request!, callback: { (tokenResponse, error) in
+                            if tokenResponse != nil && error == nil {
+                                authState.update(with: tokenResponse, error: error)
+                                print("REFRESH TOKEN DONE")
+                                self.saveAuthState(authState: authState)
+                            } else {
+                                print("REFRESH FAILED")
+                                loggedIn = false
+                                try? self.keychain.remove(self.keychainKeyAuthState)
+                            }
+                        })
+                    }
+                } else {
+                    print("MISSING EXP DATE")
+                    loggedIn = false
+                    try? keychain.remove(keychainKeyAuthState)
+                }
+            } else {
+                print("INVALID authStateData")
+                loggedIn = false
+                try? keychain.remove(keychainKeyAuthState)
+            }
+        } else {
             loggedIn = false
         }
         print("userLoggedIn \(loggedIn)")
@@ -77,10 +79,8 @@ class AuthService {
     }
     
     func logout() {
-        let keychain = Keychain(service: keychainName)
         do {
-            try keychain.remove(keychainKeyToken)
-            try keychain.remove(keychainKeyRefresh)
+            try keychain.remove(keychainKeyAuthState)
             print("LOGGED OUT")
         } catch let error {
             print("error: \(error)")
